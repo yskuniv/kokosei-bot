7bbf914f25b9ad578acdbe87475d2092d35b9f08
ysk_univ <ysk.univ.1007@gmail.com>
2020-11-29 22:13:47 +0900

    [index.ts] rewrite using core

--- index.ts
+++ index.ts
@@ -1,26 +1,15 @@
 import {
-  LineWebhookEventObject,
   EventSourceUser,
   MessageEvent,
   MessageEventTextMessage
-} from './types'
+} from './core/types'
+import {
+  generateLambdaHandler
+} from './core'
 import { sample } from './utils/misc'
 import { pushMessage } from './utils/line'
 
-exports.handler = async (event: LineWebhookEventObject) => {
-  console.log('Received event object:', event)
-
-  return Promise.all(
-    event.events
-      .filter(ev => ev.type === 'message')
-      .map(async ev => {
-        await messageEventHandler(ev as MessageEvent)
-        return 'Success!'
-      })
-  )
-}
-
-async function messageEventHandler(ev: MessageEvent): Promise<void> {
+async function jkBotMessageEventHandler(ev: MessageEvent): Promise<void> {
   const message = ev.message as MessageEventTextMessage  // TODO: Text以外のmessageに対応する
   const source = ev.source as EventSourceUser  // TODO: user以外のsourceに対応する
 
@@ -35,3 +24,5 @@ async function messageEventHandler(ev: MessageEvent): Promise<void> {
 
   await pushMessage(source.userId, replyMessage)
 }
+
+exports.handler = generateLambdaHandler(jkBotMessageEventHandler)
