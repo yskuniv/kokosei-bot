6591e711b41fd76348463a64e4660128cff44042
ysk_univ <ysk.univ.1007@gmail.com>
2020-11-29 17:12:37 +0900

    [index.ts] fix to use added utils

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import {
   MessageEvent,
   MessageEventTextMessage
 } from "./types"
+import { sample } from './utils/misc'
+import { pushMessage } from './utils/line'
 
 exports.handler = async (event: LineWebhookEventObject) => {
   console.log('event:', event)
@@ -29,78 +31,4 @@ exports.handler = async (event: LineWebhookEventObject) => {
         return 'Success!'
       })
   )
-}
-
-async function pushMessage(destId: string, msg: string): Promise<void> {
-  const accessToken = process.env.CHANNEL_ACCESS_TOKEN as string
-
-  try {
-    const res = await postJson(
-      'api.line.me',
-      '/v2/bot/message/push',
-      accessToken,
-      {
-        'messages': [
-          {
-            'type': 'text',
-            'text': msg
-          }
-        ],
-        'to': destId
-      }
-    )
-    console.log('postJson succeeded:', res)
-  } catch (error) {
-    console.error('postJson failed:', error)
-  }
-}
-
-async function postJson(hostname: string, path: string, accessToken: string, jsonParams: Object): Promise<Object> {
-  const requestBody = JSON.stringify(jsonParams)
-
-  return httpsHelper(
-    hostname,
-    path,
-    {
-      'Content-Type': 'application/json; charset=utf-8',
-      'Content-Length': Buffer.byteLength(requestBody),
-      'Authorization': 'Bearer ' + accessToken
-    },
-    'POST',
-    requestBody
-  )
-}
-
-function httpsHelper(hostname: string, path: string, headers: Object, method: string, requestBody: string): Promise<Object> {
-  const https = require('https')
-
-  const opts = {
-    hostname: hostname,
-    path: path,
-    headers: headers,
-    method: method,
-  }
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(opts, (res: any) => {
-      // res.setEncoding('utf8')
-      res.on('data', (responseBody: Object) => {
-        resolve(responseBody)
-      })
-    }).on('error', (error: Object) => {
-      reject(error)
-    })
-
-    req.write(requestBody)
-    req.end()
-  })
-}
-
-function sample(array: Array<any>): any {
-  return array[rand(array.length)]
-}
-
-function rand(n: number): number {
-  const kokuRandom = (Math.random() + Math.random() + Math.random() + Math.random() + Math.random()) / 5.0
-  return Math.floor(kokuRandom * n)
 }
