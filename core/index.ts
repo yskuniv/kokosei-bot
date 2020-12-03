04c811cf0264c4fbf608363be53b7a65fd1d5575
ysk_univ <ysk.univ.1007@gmail.com>
2020-12-03 22:09:47 +0900

    core drastic update

--- core/index.ts
+++ core/index.ts
@@ -1,12 +1,17 @@
 import {
   LineWebhookEventObject,
+  EventSourceUser,
   MessageEvent,
-  MessageEventHandler
+  MessageEventTextMessage,
+  MessageHandler
 } from './types'
+import { Line } from './utils/line'
 
 type AwsLambdaHandler = (event: {}, context: {}, callback: (...args: any[]) => void) => void
 
-export function generateAwsLambdaHandler(messageEventHandler: MessageEventHandler): AwsLambdaHandler {
+const CHANNEL_ACCESS_TOKEN: string = process.env.CHANNEL_ACCESS_TOKEN as string
+
+export function generateAwsLambdaHandler(messageHandler: MessageHandler): AwsLambdaHandler {
   return (event: {}, _context: {}, callback: (...args: any[]) => void) => {
     callback(null, 200)
 
@@ -14,11 +19,19 @@ export function generateAwsLambdaHandler(messageEventHandler: MessageEventHandle
 
     console.log('Received event object:', eventObject)
 
+    const line = new Line(CHANNEL_ACCESS_TOKEN)
+
     eventObject.events
       .forEach(async ev => {
         if (ev.type === 'message') {
           const ev_ = ev as MessageEvent
-          messageEventHandler(ev_)
+          const source = ev_.source as EventSourceUser  // TODO: user以外のsourceに対応する
+          const message = ev_.message as MessageEventTextMessage  // TODO: Text以外のmessageに対応する
+
+          messageHandler(
+            message.text,
+            async (text: string) => { await line.pushMessage(source.userId, text) }
+          )
         } else {
           // do nothing
         }
