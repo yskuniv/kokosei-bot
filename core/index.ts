b44ece631544930a82009a4e022152dc59568e1b
ysk_univ <ysk.univ.1007@gmail.com>
2020-12-04 22:18:10 +0900

    [core/index.ts] small adjust

--- core/index.ts
+++ core/index.ts
@@ -11,10 +11,10 @@ const CHANNEL_ACCESS_TOKEN: string = process.env.CHANNEL_ACCESS_TOKEN as string
 
 export function generateAwsLambdaHandler(messageHandler: MessageHandler): AwsLambdaHandler {
   return (event: {}, _context: {}, callback: (...args: any[]) => void) => {
+    // return response first
     callback(null, 200)
 
     const eventObject = event as LineWebhookEventObject
-
     console.log('Received event object:', eventObject)
 
     const line = new Line(CHANNEL_ACCESS_TOKEN)
